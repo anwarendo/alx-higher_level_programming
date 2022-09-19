aec10fe64d44ec98b15289e9bc4ba8d370105008
Anwar Endris <anwarendo@gmail.com>
2022-09-19 16:51:51 +0300

    Add task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ *
+ * @list: pointer for the list data structure
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	(void) list;
+
+	return (0);
+}
